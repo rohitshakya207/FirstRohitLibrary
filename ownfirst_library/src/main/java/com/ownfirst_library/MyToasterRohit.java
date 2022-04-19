7729347b0056c09427185f6ab6f04c5e14ada656
rohitshakya207 <rohitshakya207@gmail.com>
2022-04-19 17:19:21 +0530

    commit exa

---
+++ ownfirst_library/src/main/java/com/ownfirst_library/MyToasterRohit.java
@@ -0,0 +1,12 @@
+package com.ownfirst_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyToasterRohit {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
